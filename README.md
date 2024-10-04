593c73a244f14d6a6947a3bbeafb299919056f4c
damooola <damolanelson@gmail.com>
2024-10-04 22:03:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# weather_app
+
+A new Flutter project.
+
+## Getting Started
+
+read me be completed when project is done
---
+++ lib/services/weather_services.dart
@@ -0,0 +1,4 @@
+class WeatherServices {
+  static  const BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
+  final String apiKey;
+}
